d384e006b22f6f92cc144d229624b05b7bdab345
Galen Chen <ywcctc@gmail.com>
2018-07-02 09:46:26 -0400

    ddd

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,30 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    // strore candidate
-    // read candidate
-    string public candidate;
+  
+    // model  a  candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //store candidates
+    //fetch candidate
+
+    mapping (uint => Candidate) public candidates;
+    //store candidates count   ..becasue we can not know the size of map; so use it to count.
+    uint public candidatesCount;
+
     //constructor
-    function Election () public {
-        candidate = "Candidate 1";
-        
+    function Election () public {   
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name,0); //add this candidate to number cC in the map
     }
 
 
